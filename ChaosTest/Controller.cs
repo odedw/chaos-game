eebb3e17a5f8969d420d945bec7160f80b746399
Oded Welgreen <oded_wel@hotmail.com>
2014-05-10 12:04:07 +0300

    initial commit

---
+++ ChaosTest/Controller.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ChaosTest
+{
+    public class Controller : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Event raised to notify that a property has changed
+        /// </summary>
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">The property name</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+
+        private bool mIsRunning;
+
+        public bool IsRunning
+        {
+            get { return mIsRunning; }
+            set
+            {
+                if (value == mIsRunning)
+                    return;
+                mIsRunning = value;
+
+                OnPropertyChanged("IsRunning");
+                OnPropertyChanged("IsReady");
+
+            }
+        }
+
+        public bool IsReady
+        {
+            get { return !mIsRunning; }
+        }
+
+        private int mInterval = 1;
+
+        public int Interval
+        {
+            get { return mInterval; }
+            set
+            {
+                if (value == mInterval)
+                    return;
+                mInterval = value;
+
+                OnPropertyChanged("Interval");
+            }
+        }
+
+        private double mDenominator = 2;
+
+        public double Denominator
+        {
+            get { return mDenominator; }
+            set
+            {
+                if (value == mDenominator)
+                    return;
+                mDenominator = value;
+
+                OnPropertyChanged("Denominator");
+            }
+        }
+
+        private int mNumberOfVertices = 3;
+
+        public int NumberOfVertices
+        {
+            get { return mNumberOfVertices; }
+            set
+            {
+                if (value == mNumberOfVertices)
+                    return;
+                mNumberOfVertices = value;
+
+                OnPropertyChanged("NumberOfVertices");
+            }
+        }
+
+        List<Point> mVertices = new List<Point>();
+        public Point TowardsVertix { get; set; }
+        public Point LastPoint { get; set; }
+
+        private int mWidth;
+        private int mHeight;
+        Random mRandom = new Random();
+
+        #endregion
+
+        public static event Action<int, int> DrawPixel;
+
+        public void Run(int height, int width)
+        {
+            IsRunning = true;
+            mHeight = height;
+            mWidth = width;
+            CalcPolygon();
+                //new List<Point>()
+            //                {
+            //                    new Point(0,height),
+            //                    new Point((float)width/2,0),
+            //                    new Point(width,height)
+            //                    //new Point(0,0),
+            //                    //new Point(width,height),
+            //                    //new Point(0,height),
+            //                    //new Point(width,0)
+
+
+            //                };
+            LastPoint = new Point((float)width / 2, (float)height / 2);
+            new Thread(DoWork) {IsBackground = true, Name = "WorkerThread"}.Start();
+        }
+
+        private void CalcPolygon()
+        {
+            mVertices = new List<Point>();
+            var angle = 2*Math.PI/NumberOfVertices;
+            double mid = ((double) mHeight/2);
+            for (int i = 0; i < NumberOfVertices; i++)
+            {
+                var x = mid + mid * Math.Sin(i * angle);
+                var y = mid + mid * Math.Cos(i * angle);
+                mVertices.Add(new Point(x,y));
+            }
+        }
+
+        public void Reset()
+        {
+            IsRunning = false;
+        }
+
+        private void DoWork()
+        {
+            while (mIsRunning)
+            {
+                Point nextPoint = CalcNextPoint();
+                int x = (int) nextPoint.X;
+                int y = (int) nextPoint.Y;
+                if (x > mWidth || y > mHeight)
+                    Debugger.Break();
+                if (DrawPixel != null)
+                    MainWindow.CurrentDispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(() => DrawPixel(x, y)));
+                LastPoint = nextPoint;
+
+                Thread.Sleep(mInterval);
+            }
+        }
+
+        private Point CalcNextPoint()
+        {
+            TowardsVertix = mVertices[mRandom.Next(mVertices.Count)];
+            Point distance = new Point((Math.Max(TowardsVertix.X,LastPoint.X) - Math.Min(TowardsVertix.X,LastPoint.X)) * (1 - 1 / Denominator),
+                                       (Math.Max(TowardsVertix.Y,LastPoint.Y) - Math.Min(TowardsVertix.Y,LastPoint.Y)) * (1 - 1 / Denominator));
+            Point nextPoint = new Point(LastPoint.X + distance.X * Math.Sign(TowardsVertix.X - LastPoint.X),
+                                        LastPoint.Y + distance.Y * Math.Sign(TowardsVertix.Y - LastPoint.Y));
+            return nextPoint;
+        }
+
+    }
+}
---
+++ ChaosTest/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ChaosTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller Controller
+        {
+            get { return DataContext as Controller; }
+        }
+
+        private int mColorData;
+
+        public static Dispatcher CurrentDispatcher { get; set; }
+
+        static public WriteableBitmap Bitmap { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Controller();
+            Controller.DrawPixel += DrawPixel;
+            CurrentDispatcher = this.Dispatcher;
+            // Compute the pixel's color.
+            mColorData = 255 << 16; // R
+            mColorData |= 255 << 8;   // G
+            mColorData |= 255 << 0;   // B
+
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (Controller.IsRunning)
+            {
+                btnReset_Click(null, e);
+                Thread.Sleep(100);
+            }
+            Controller.Run((int)Bitmap.Width, (int)Bitmap.Height);
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.Reset();
+            InitBitmap();
+        }
+
+        private void Border_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            InitBitmap();
+        }
+
+        private void InitBitmap()
+        {
+            Bitmap = new WriteableBitmap(
+                (int) imgContainer.ActualWidth,
+                (int) imgContainer.ActualHeight,
+                96,
+                96,
+                PixelFormats.Bgr32,
+                null);
+            img.Source = Bitmap;
+        }
+
+        // The DrawPixel method updates the WriteableBitmap by using
+        // unsafe code to write a pixel into the back buffer.
+        public void DrawPixel(int x, int y)
+        {
+            if (!Controller.IsRunning)
+                return;
+            int column = x;
+            int row = y;
+
+            // Reserve the back buffer for updates.
+            Bitmap.Lock();
+
+            unsafe
+            {
+                // Get a pointer to the back buffer.
+                int pBackBuffer = (int)Bitmap.BackBuffer;
+
+                // Find the address of the pixel to draw.
+                pBackBuffer += row * Bitmap.BackBufferStride;
+                pBackBuffer += column * 4;
+
+                
+
+                // Assign the color data to the pixel.
+                *((int*)pBackBuffer) = mColorData;
+            }
+
+            // Specify the area of the bitmap that changed.
+            Bitmap.AddDirtyRect(new Int32Rect(column, row, 1, 1));
+
+            // Release the back buffer and make it available for display.
+            Bitmap.Unlock();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Equals(sender, btnDenominatorUp))
+                Controller.Denominator++;
+            else if (Equals(sender, btnDenominatorDown))
+                Controller.Denominator--;
+            else if (Equals(sender, btnVerticesUp))
+                Controller.NumberOfVertices++;
+            else if (Equals(sender, btnVerticesDown))
+                Controller.NumberOfVertices--;
+            btnRun_Click(null,e);
+        }
+
+    }
+}
